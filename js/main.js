610209c8cd0e36bcf6ae8d7f178ce22ea255840a
Patrick Best <patrickbest@patrickbest.com>
2016-12-08 13:14:59 -0600

    set higher threshold for mobile site
    for landscape ipad

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var Site = {
     this.windowWidth = $(window).innerWidth();
     this.windowHeight = $(window).innerHeight();
     this.marginBasic = parseInt($('#main-container').css('padding-top'));
-    this.mobileWidth = 800;
+    this.mobileWidth = 1024;
     this.fullWidth = 1487;
 
     this.bind();
